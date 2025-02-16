6ab26517576591088b15ea4c5eecb67b04dda743
Ignorant View <theignorant.views@gmail.com>
2025-02-16 20:53:34 +0000

    moved files to root directory

--- skillanthropy/app/components/navigation/ListOptions.tsx
+++
@@ -1,3 +0,0 @@
-export default function ListOptions({ option }: { option: string }) {
-  return <li className="">{option}</li>;
-}
--- skillanthropy/app/routes/logout.ts
+++
@@ -1,6 +0,0 @@
-import type { ActionFunction, LoaderFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { logout } from "~/services/session.server";
-
-export const action: ActionFunction = async ({ request }) => logout(request);
-export const loader: LoaderFunction = async () => redirect("/login");
